35e6e853478a73fb066b30f227bdf91fc100d315
MD Islam <mdislam@live.com>
2025-01-26 11:22:27 -0500

    updated files added!

--- src/test/java/test/LoginPageTest.java
+++ src/test/java/test/LoginPageTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import config.ConfigReader;
 import page.LoginPage;
@@ -12,21 +15,30 @@ import page.TestBase;
 
 public class LoginPageTest extends TestBase {
 	LoginPage loginPageObj;
+	Logger logger = LogManager.getLogger(LoginPageTest.class);
+	SoftAssert softAssert;
 
 	@BeforeMethod
 	public void setUp() {
 		initDriver();
 		driver.get(ConfigReader.getProperty("base.url"));
 		loginPageObj = PageFactory.initElements(driver, LoginPage.class);
+		softAssert= new SoftAssert();
 	}
 
 	@Test
 	public void loginTest() {
-		loginPageObj.enterUserName(ConfigReader.getProperty("username"));		
-		System.out.println("print something!");
+		loginPageObj.enterUserName(ConfigReader.getProperty("username"));			
 		loginPageObj.enterPassword(ConfigReader.getProperty("password"));
 		loginPageObj.clickSignInButton();
+		String expectedTitle = "Codefios2";
+		String actualTitle = loginPageObj.getPageTitle();
 		takeScreenshot(driver);
+		logger.info("This is beforeAssert!");
+//		Assert.assertEquals(actualTitle, expectedTitle);
+		softAssert.assertEquals(actualTitle, expectedTitle);		
+		logger.info("This is afterAssert!");		
+		softAssert.assertAll();
 	}
 
 	@Test
@@ -34,16 +46,20 @@ public class LoginPageTest extends TestBase {
 		loginPageObj.enterUserName(ConfigReader.getProperty("username"));
 		loginPageObj.enterPassword(ConfigReader.getProperty("password"));
 		loginPageObj.clickSignInButton();
+		takeScreenshot(driver);
 		String expectedTitle = "Codefios";
 		String actualTitle = loginPageObj.getPageTitle();
-		Assert.assertEquals(actualTitle, expectedTitle);
-		takeScreenshot(driver);
+		logger.info("This is beforeAssert!");
+		softAssert.assertEquals(actualTitle, expectedTitle);
+		logger.info("This is afterAssert!");
+		softAssert.assertAll();
+		
 	}
 
 	@AfterMethod
 	public void tearDown() {
 		driver.close();
-		driver.quit();
+		driver.quit();		
 	}
 
 }
